07ef8c39e91f6561a804d3455da5ef0031523e1b
Stratton <113083875+StrattonJJ@users.noreply.github.com>
2024-05-12 10:56:29 +1000

    Reverted
    Reverted files

--- Assignment_2/forward_chaining.py
+++ Assignment_2/forward_chaining.py
@@ -2,8 +2,8 @@ class Forward:
     def __init__(self, kb, query) -> None:
         """
         Initialize the Forward Chaining object.
-        :param kb: The parsed knowledge base.
-        :param query: The parsed query.
+        :param kb: The knowledge base as a list of rules.
+        :param query: The query to be inferred.
         """
         self.kb = kb
         self.query = query
@@ -13,24 +13,16 @@ class Forward:
         Perform Forward Chaining inference.
         :return: A string indicating the result of the inference.
         """
-        # Print the parsed knowledge base and query
-        print("Parsed Knowledge Base:")
-        for fact_or_rule in self.kb:
-            print(fact_or_rule)
-        print("\nParsed Query:")
-        print(self.query)
-        print()
-
         # List to store the inferred facts in the order they were inferred
         inferred = []
         # List to store the facts to be processed (agenda)
         agenda = []
 
         # Add initial facts to the agenda and inferred list
-        for fact in self.kb:
-            if fact['type'] == 'proposition':
-                agenda.append(fact['symbol']['symbol'])
-                inferred.append(fact['symbol']['symbol'])
+        for rule in self.kb:
+            if "=>" not in rule:
+                agenda.append(rule)
+                inferred.append(rule)
 
         # Process facts from the agenda
         while agenda:
@@ -38,29 +30,27 @@ class Forward:
 
             # Generate new facts based on the rules
             for rule in self.kb:
-                if rule['type'] == 'implication':
-                    antecedent = rule['antecedent']
-                    consequent = rule['consequent']
+                if "=>" in rule:
+                    antecedent, consequent = rule.split("=>")
+                    antecedent = antecedent.strip()
+                    consequent = consequent.strip()
 
                     # Check if the antecedent is a conjunction of multiple facts
-                    if antecedent['type'] == 'conjunction':
-                        antecedent_facts = set(fact['symbol'] for fact in antecedent['arguments'])
-                        if all(f in inferred for f in antecedent_facts) and consequent['symbol'] not in inferred:
-                            agenda.append(consequent['symbol'])
-                            inferred.append(consequent['symbol'])
+                    if "&" in antecedent:
+                        antecedent_facts = set(antecedent.split("&"))
+                        # If all antecedent facts are inferred and the consequent is not inferred,
+                        # add the consequent to the agenda and inferred list
+                        if antecedent_facts.issubset(set(inferred)) and consequent not in inferred:
+                            agenda.append(consequent)
+                            inferred.append(consequent)
                     # If the antecedent is a single fact and it is inferred,
                     # add the consequent to the agenda and inferred list
-                    elif antecedent['type'] == 'atomic' and antecedent['symbol'] in inferred and consequent['symbol'] not in inferred:
-                        agenda.append(consequent['symbol'])
-                        inferred.append(consequent['symbol'])
-
-        # Print the inferred facts
-        print("Inferred Facts:")
-        print(inferred)
-        print()
+                    elif antecedent in inferred and consequent not in inferred:
+                        agenda.append(consequent)
+                        inferred.append(consequent)
 
         # Check if the query is in the inferred list
-        if self.query['type'] == 'atomic' and self.query['symbol'] in inferred:
+        if self.query in inferred:
             # If the query is inferred, return "YES" along with the inferred facts in the order they were inferred
             return "YES: " + ", ".join(inferred)
         else:
